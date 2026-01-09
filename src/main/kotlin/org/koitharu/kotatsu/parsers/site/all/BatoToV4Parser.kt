4cc055390ca30f0269f8f9c6963a5721722d66ab
dragonx943 <premieregirl26@gmail.com>
2026-01-09 18:25:03 +0700

    fix(Bato.To v4): Custom graphQLQuery & POST method for requests

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/all/BatoToV4Parser.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/all/BatoToV4Parser.kt
@@ -1,8 +1,11 @@
 package org.koitharu.kotatsu.parsers.site.all
 
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 import org.json.JSONArray
 import org.json.JSONObject
@@ -233,12 +236,29 @@ internal class BatoToV4Parser(context: MangaLoaderContext) :
 		return chain.proceed(request)
 	}
 
+	// Custom httpPost & graphQLQuery function for requests
 	private suspend fun graphQLQuery(endpoint: String, query: String, variables: JSONObject): JSONObject {
 		val payload = JSONObject().apply {
 			put("query", query)
 			put("variables", variables)
 		}
-		val response = webClient.httpPost(endpoint.toHttpUrl(), payload, getRequestHeaders())
+
+		val mediaType = "application/json; charset=utf-8".toMediaType()
+		val requestBody = payload.toString().toRequestBody(mediaType)
+		val headers = getRequestHeaders().newBuilder()
+			.set("Content-Type", "application/json")
+			.removeAll("Content-Encoding")
+			.removeAll("Accept-Encoding")
+			.build()
+
+		val request = Request.Builder()
+			.post(requestBody)
+			.url(endpoint.toHttpUrl())
+			.headers(headers)
+			.tag(MangaParserSource::class.java, source)
+			.build()
+
+		val response = context.httpClient.newCall(request).await()
 		val json = response.parseJson()
 		json.optJSONArray("errors")?.let {
 			if (it.length() != 0) {
